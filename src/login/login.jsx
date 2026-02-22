281d0dfc088d6c2f07792da2e7614be6f094464e
Kou Barlow <kougbarlow@gmail.com>
2026-02-21 19:52:19 -0700

    working on login page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,14 +5,23 @@ import { useNavigate } from "react-router-dom";
 
 export function Login() {
   const navigate = useNavigate();
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [displayError, setDisplayError] = React.useState(null);
+
+  async function loginUser() {
+    localStorage.setItem('email', email)
+  }
+
   return (
     <main>
       <div class="loginCard">
         <h2>Login</h2>
         <form method="get" action="explore">
-          <input name="email" type="text" placeholder="your@email.com" />
-          <input name="password" type="password" placeholder="password" />
-          <button class="login-button" type="submit">Login</button>
+          <input name="email" type="text" onChange={(e) => setEmail(e.target.value)} placeholder="your@email.com" />
+          <input name="password" type="password" onChange={(p) => setPassword(p.target.value)} placeholder="password" />
+          <button class="login-button" type="submit" onClick={() => loginUser()} disabled={!email || !password}>Login</button>
           <a href="/signup">Sign Up</a>
         </form>
       </div>
