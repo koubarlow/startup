715e3071227795c32d20909477afd0bed047d9f2
Kou Barlow <kougbarlow@gmail.com>
2026-02-26 19:44:06 -0700

    explore uses json data from mock db

--- src/explore/explore.jsx
+++ src/explore/explore.jsx
@@ -1,8 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { JournalDetailModal } from './journalDetailModal';
+import journalData from './journals.json';
+import userData from './users.json';
+
 export function Explore() {
 
   const [showJournalDetail, setShowJournalDetail] = useState(false);
+  const [journals, setJournals] = useState(journalData?.journals || []);
+
+  function ExploreEntry({ journalEntry }) {
+    let entry = journalEntry.entry.substring(0, 100);
+    let topic = journalEntry.topic.substring(0, 20);
+    let reads = journalEntry.reads.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    let journalUser = userData.users.find(user => user.userId === journalEntry.userId);
+    let username = journalUser.username;
+
+    return(
+      <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
+        <div className="flex justify-between">
+          <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">{topic}</h5>
+          <p className="text-body mb-6">{reads} reads</p>
+        </div>
+        <p className="text-body mb-6">{entry}...</p>
+        <p className="font-semibold float-right">{username}</p>
+      </div>
+    );
+  }
+  
   return (
     <main>
       {showJournalDetail && (
@@ -12,121 +36,12 @@ export function Explore() {
         )}
       <h1>Explore</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mx-8 my-4">
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Housing</h5>
-            <p className="text-body mb-6">20.4k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇯🇵 晃くん</p>
-        </div>
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Work Stress</h5>
-            <p className="text-body mb-6">14.7k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇺🇸 Timothy Dale</p>
-        </div>
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Cost of Living</h5>
-            <p className="text-body mb-6">20.4k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇺🇸 Dylan Cook</p>
-        </div>
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Daily Life</h5>
-            <p className="text-body mb-6">20.4k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇯🇵 田中さん</p>
-        </div>
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Emotional Well-being</h5>
-            <p className="text-body mb-6">20.4k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇯🇵 ポーさん</p>
-        </div>
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Weather</h5>
-            <p className="text-body mb-6">20.4k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇨🇦 Yanny Bokes</p>
-        </div>
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Pop Music</h5>
-            <p className="text-body mb-6">20.4k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇲🇲 Min</p>
-        </div>
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">K-Pop Demon Hunters</h5>
-            <p className="text-body mb-6">20.4k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇰🇷 Sung Hung</p>
-        </div>
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">University</h5>
-            <p className="text-body mb-6">20.4k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇯🇵 桜</p>
-        </div>
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Pho Noodles</h5>
-            <p className="text-body mb-6">20.4k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇺🇸 Joanna Barlow</p>
-        </div>
-        <div onClick={() => setShowJournalDetail(prev => !prev)} className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs transition-transform duration-200 hover:scale-105">
-          <div className="flex justify-between">
-            <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">Surfing</h5>
-            <p className="text-body mb-6">20.4k</p>
-          </div>
-          <p className="text-body mb-6">アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。</p>
-          <p className="font-semibold float-right">🇦🇺 Joseph Jeffrey</p>
-        </div>
-      </div>
-  
-      <div className="modal">
-        <div className="card">
-          <h2>🇯🇵 晃くん's Journal - Jan. 27, 2026</h2>
-          <h3>Housing</h3>
-          <p>アパートに住んでいます。毎日、周りが静かで落ち着いた雰囲気を感じながら生活しています。
-              朝や夜もあまり騒音がなく、自分の時間を大切にできる環境です。
-
-              部屋の大きさはあまり広くなく、少し狭いと感じることもありますが、その分掃除はしやすいです。
-              窓からは小さな公園を見ることができ、天気のいい日は子どもたちが遊んでいる様子が見えます。
-              その景色を見ると、気持ちが少し和らぎます。
-
-              家賃は70000円で、毎月の支払いを工夫しながら生活しています。
-              無駄な出費を減らしたり、食費を考えたりして、できるだけ節約するようにしています。
-
-              近所にはスーパーや図書館があり、とても便利です。
-              スーパーは歩いて行ける距離にあり、図書館では静かな時間を過ごすことができます。
-
-              もし可能なら、将来はもっと広い公園の近くに住みたいです。
-              自然が多く、散歩ができる場所があると、今よりもっとリラックスできると思います。
-
-              今日の気持ちはとても落ち着いています。
-              静かな生活のおかげで、心が安定していると感じます。
-          </p>
-          <a href="explore.html">Translate</a>
-        </div>
+        {journals.map(entry => (
+          <ExploreEntry
+            key={entry.journalId}
+            journalEntry={entry}
+          />
+        ))}
       </div>
     </main>
   );
